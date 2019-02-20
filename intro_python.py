55bcd4822ba1fb4e178b174921786c29c8da1826
sami <jamadar.samiulla@gmail.com>
2019-02-20 08:52:52 +0530

    Class 20022019 7:30-8:51

---
+++ intro_python.py
@@ -0,0 +1,33 @@
+def sum(a,b):
+    return a+b
+
+s = sum(4,6)    
+
+def add_arr(l,m):
+    return l+m
+
+# l = [1,2,3,'a','b']
+# t = (1,2,3,'a','b')
+# l[0] = 66
+
+# d = dict()
+# d['name']='sam'
+# d['age']= 28
+# print(d)
+
+# #name = sami and age = 28
+# for k in d.keys():
+#     print(d[k])
+
+
+# a = [1,2,3,1,6]
+# A = sorted(a)
+# print(A)
+# b = [4,5,6]
+# B = sorted(b)
+# print(B)
+
+# print(add_arr(a,b))
+
+
+
---
+++ loop.py
@@ -0,0 +1,36 @@
+arr = [1,2,3,4,5]
+for x in arr:
+    # print(x)
+    pass
+
+for x in range(6):
+    # print(x)
+    pass
+
+# print(range[1:10])
+for x in range(1,10):
+    # print (x)
+    pass
+
+for x in range (1,10,3):
+    # print(x)
+    pass
+
+count = 0
+while True:
+    print(count)
+    count += 1
+    break
+    # if count >= 5:
+
+
+for x in range(10):
+    if x%2 ==0:
+        continue
+    elif x==5:
+        continue
+    else:
+        print(x)   
+
+        
+
