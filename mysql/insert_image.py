4df81f2fec7422d43b10465224e3f0022724f807
sami <samiulla.mahamad@ptgindia.com>
2019-03-04 09:48:01 +0530

    add and reade image modification

--- mysql/insert_image.py
+++ mysql/insert_image.py
@@ -1,10 +1,19 @@
 import MySQLdb as mdb
+import time
 def read_image():
-    with open("download.jpeg",'rb') as f:
-        img = f.read()
-        return img
+	with open("download.jpeg",'rb') as f:
+		img = f.read()
+		return img
 con = mdb.connect('localhost', 'testuser', 'test623', 'testdb')
+#Create database table here for image storing
+# with con:
+# 	cur = con.cursor()
+# 	cur.execute("CREATE TABLE IF NOT EXISTS Image (id INT PRIMARY KEY AUTO_INCREMENT, \
+# 	photo BLOB NOT NULL, name VARCHAR(500))")
 with con:
-    cur = con.cursor()
-    data = read_image()
-    cur.execute("INSERT INTO Images VALUES(1, %s)", (data, ))
+	cur = con.cursor()
+	data = read_image()
+	millis = int(round(time.time() * 1000))
+	cur.execute("INSERT INTO Image (photo,name) VALUES(%s, %s)", \
+		(data, 'first_image'+str(millis)+'.jpg'))
+
--- mysql/read_image.py
+++ mysql/read_image.py
@@ -1,11 +1,22 @@
 import MySQLdb as mdb 
-def writeImage(data):
-    fout = open('sid2.jpg', 'wb')
+
+def writeImage(data,name):
+    fout = open(name, 'wb')
     with fout:
         fout.write(data)
+
+
 con = mdb.connect('localhost', 'testuser', 'test623', 'testdb')
 with con:
-    cur = con.cursor()
-    cur.execute("SELECT Data FROM Images WHERE Id=1")
-    data = cur.fetchone()[0]
-    writeImage(data)  
+    cur = con.cursor(mdb.cursors.DictCursor)
+    cur.execute("SELECT photo,name FROM Image WHERE id=1")
+    data = cur.fetchone()
+    writeImage(data['photo'],data['name'])  
+
+
+create
+insert 
+update
+delete
+
+images
